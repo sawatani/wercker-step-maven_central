8ae9ded149e4e37543787626a92410a66780e3ea
Kunio Sawatani <devel@fathens.org>
2015-02-13 12:59:33 +0900

    pomExtra をログに残す

--- run.sh
+++ run.sh
@@ -1,5 +1,27 @@
 . ./setup.sh
 
+pomExtra="<url>${WERCKER_MAVEN_CENTRAL_PROJECT_URL}</url>
+  <licenses>
+    <license>
+      <name>${WERCKER_MAVEN_CENTRAL_LICENSE_NAME}</name>
+      <url>${WERCKER_MAVEN_CENTRAL_LICENSE_URL}</url>
+      <distribution>repo</distribution>
+    </license>
+  </licenses>
+  <scm>
+    <url>${WERCKER_MAVEN_CENTRAL_SCM_URL}</url>
+    <connection>${WERCKER_MAVEN_CENTRAL_SCM_CONNECTION}</connection>
+  </scm>
+  <developers>
+    <developer>
+      <id>${WERCKER_MAVEN_CENTRAL_DEVELOPER_ID}</id>
+      <name>${WERCKER_MAVEN_CENTRAL_DEVELOPER_NAME}</name>
+      <url>${WERCKER_MAVEN_CENTRAL_DEVELOPER_URL}</url>
+    </developer>
+  </developers>"
+
+echo "POM Extra: $pomExtra"
+
 mkdir -vp project
 cat <<EOF >> project/plugins.sbt
  
@@ -29,26 +51,7 @@ publishArtifact in Test := false
  
 pomIncludeRepository := { _ => false }
  
-pomExtra := (
-  <url>https://github.com/sawatani/Astronomy</url>
-  <licenses>
-    <license>
-      <name>${WERCKER_MAVEN_CENTRAL_LICENSE_NAME}</name>
-      <url>${WERCKER_MAVEN_CENTRAL_LICENSE_URL}</url>
-      <distribution>repo</distribution>
-    </license>
-  </licenses>
-  <scm>
-    <url>${WERCKER_MAVEN_CENTRAL_SCM_URL}</url>
-    <connection>${WERCKER_MAVEN_CENTRAL_SCM_CONNECTION}</connection>
-  </scm>
-  <developers>
-    <developer>
-      <id>${WERCKER_MAVEN_CENTRAL_DEVELOPER_ID}</id>
-      <name>${WERCKER_MAVEN_CENTRAL_DEVELOPER_NAME}</name>
-      <url>${WERCKER_MAVEN_CENTRAL_DEVELOPER_URL}</url>
-    </developer>
-  </developers>)
+pomExtra := (${pomExtra})
  
 credentials += Credentials("Sonatype Nexus Repository Manager", 
                            "oss.sonatype.org", 
